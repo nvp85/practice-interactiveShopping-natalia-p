fa4a2b1dec68f107ce66a7472b4b864159007bf5
nvp85 <nvp@zhukov.al>
2025-05-07 14:41:07 -0500

    add more comments

--- shopping-list.js
+++ shopping-list.js
@@ -4,22 +4,26 @@ const list = document.getElementById("list");
 
 let itemsCounter = 0; // to generate ids for list elements
 
+addButton.addEventListener("click", addListElement);
+
 function addListElement() {
+    // create a new list item
     const newElement = document.createElement("li");
     newElement.textContent = inputField.value;
     newElement.id = itemsCounter; // each list element has a unique id
     itemsCounter++; 
-    list.appendChild(newElement);
-
+    list.appendChild(newElement); // the item is added to the list
+    // create a remove button
     const removeButton = document.createElement("button");
     removeButton.textContent = "remove";
     removeButton.value = newElement.id; // each remove button will be connected to an element via its id
-    newElement.appendChild(removeButton);
+    newElement.appendChild(removeButton); // the remove button is added to the list item
     removeButton.addEventListener("click", (event) => removeItem(event));
+    // create an edit button
     const editButton = document.createElement("button");
     editButton.textContent = "edit";
     editButton.value = newElement.id; // each edit button will be connected to an element via its id
-    newElement.appendChild(editButton);
+    newElement.appendChild(editButton); // the edit button is added to the list item
     editButton.addEventListener("click", (event) => editListItem(event));
 }
 
@@ -30,24 +34,29 @@ function removeItem(event) {
 
 function editListItem(event) {
     const id = event.target.value;
-    const item = document.getElementById(event.target.value);
+    const item = document.getElementById(id); // the item is obtained by its id
+    // create an input field that will be used to edit the item's text
     const editInput = document.createElement("input");
     editInput.type = "text";
+    // place the original item's text into the input field so we could edit it
     editInput.value = item.childNodes[0].textContent; // text only node
-    item.firstChild.replaceWith(editInput); // replace the text with an input field
-    item.childNodes[1].style.display = "none"; // hide the remove and edit buttons
+    // replace the text node with the input field
+    item.firstChild.replaceWith(editInput); 
+    // hide the remove and edit buttons
+    item.childNodes[1].style.display = "none"; 
     item.childNodes[2].style.display = "none";
+    // create the save button that allows us to save the change we made
     const saveButton = document.createElement("button");
     saveButton.textContent = "save";
     item.appendChild(saveButton);
-
+    // add event listener to the save button
     saveButton.addEventListener("click", function() {
-        item.replaceChild(document.createTextNode(editInput.value), editInput); // replace the edit input with the new text
-        item.childNodes[1].style.display = "inline"; // show the buttons again
+        // replace the edit input field with the new text
+        item.replaceChild(document.createTextNode(editInput.value), editInput); 
+        // show the buttons again
+        item.childNodes[1].style.display = "inline"; 
         item.childNodes[2].style.display = "inline"; 
-        item.removeChild(saveButton); // remove the save button
+        // remove the save button
+        item.removeChild(saveButton); 
     });
 }
-addButton.addEventListener("click", addListElement);
-
-
